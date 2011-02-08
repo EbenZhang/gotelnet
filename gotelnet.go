d48cb1c87cb4d12f995829b3e43a9c40da6b4e4b
ebenzhang <ebenzhang@ubuntu.(none)>
2011-02-08 16:00:45 +0800

    safe quit, rename same variables

--- gotelnet.go
+++ gotelnet.go
@@ -6,98 +6,109 @@ import "os"
 import "strconv"
 
 
-type TelnetCommandHandler interface{
-    Handle(commandLine string)
+type TelnetCommandHandler interface {
+	Handle(commandLine string)
 }
 
-type GoTelnet struct{
-    Promote string
-    Ip string
-    StartPort uint16
-    commandHandler TelnetCommandHandler
-    preConnection *net.Conn
+type GoTelnet struct {
+	Promote           string
+	Ip                string
+	StartPort         uint16
+	commandHandler    TelnetCommandHandler
+	preConnection     *net.Conn
+	chanForQuit       chan bool //用来接收退出消息
+	chanForQuitOK     chan bool //用来反馈退出结果
+	chanForNextAccept chan bool //用来accept下一个连接
 }
 
-func NewGoTelnet(promote string, ip string, startPort uint16, handler TelnetCommandHandler) *GoTelnet{
-    tempPromote := promote
-    if len(tempPromote) == 0{
-        tempPromote = "telnet"
-    }
-    tempIp := ip
-    if len(tempIp) == 0{
-        tempIp = "0.0.0.0"
-    }
-    tempStartPort := startPort
-    if tempStartPort == 0{
-        tempStartPort = 2500
-    }
-    
-    return &GoTelnet{tempPromote,tempIp,tempStartPort,handler,nil}
+func NewGoTelnet(promote string, ip string, startPort uint16, handler TelnetCommandHandler) *GoTelnet {
+	var server GoTelnet
+	server.Promote = promote
+	if len(promote) == 0 {
+		server.Promote = "telnet"
+	}
+	server.Ip = ip
+	if len(ip) == 0 {
+		server.Ip = "0.0.0.0"
+	}
+	server.StartPort = startPort
+	if startPort <= 1024 {
+		server.StartPort = 2500
+	}
+
+	server.chanForQuit = make(chan bool, 1)
+	server.chanForQuitOK = make(chan bool, 1)
+	server.chanForNextAccept = make(chan bool, 1)
+
+	return &server
 }
 
-func (obj * GoTelnet)getAddr() *net.TCPAddr{
-    addrString := obj.Ip + ":" + strconv.Uitoa(uint(obj.StartPort))
-    tcpAddr,_ := net.ResolveTCPAddr(addrString)
-    return tcpAddr
+func (server *GoTelnet) getAddr() *net.TCPAddr {
+	addrString := server.Ip + ":" + strconv.Uitoa(uint(server.StartPort))
+	tcpAddr, _ := net.ResolveTCPAddr(addrString)
+	return tcpAddr
 }
 
-func (obj * GoTelnet)handler(listener *net.TCPListener,chanForAcceptOK chan bool){
-    conn,_ := listener.Accept()
-    chanForAcceptOK <- true
-    if obj.preConnection != nil{
-       (*obj.preConnection).Close() 
-    }
-    obj.preConnection = &conn
-    conn.Write([]byte("welcome to telnet debug server"))
+func (server *GoTelnet) acceptConnection(listener *net.TCPListener) {
+	conn, _ := listener.Accept()
+	server.chanForNextAccept <- true
+	if server.preConnection != nil {
+		(*server.preConnection).Close()
+	}
+	server.preConnection = &conn
+	conn.Write([]byte("welcome to telnet debug server"))
 
-    defer conn.Close()
-    
-    for {
-        buf := make([]byte, 1024)
-        readlen, ok := conn.Read(buf)
-        if ok != nil {
-            fmt.Fprintf(os.Stderr, "close connection when reading from socket: %s\n", ok.String())
-            return
-        }
-        if readlen == 0 {
-            fmt.Printf("Connection closed by remote host\n")
-            return
-        }
-        if buf[0] == 13{
-            conn.Write([]byte(obj.Promote + ">"))
-        }
-        if string(buf[0:3]) == "bye"{
-            conn.Write(buf)
-            break
-        }
-        obj.commandHandler.Handle(string(buf))
-    }
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		readlen, ok := conn.Read(buf)
+		if ok != nil {
+			fmt.Fprintf(os.Stderr, "close connection when reading from socket: %s\n", ok.String())
+			return
+		}
+		if readlen == 0 {
+			fmt.Printf("Connection closed by remote host\n")
+			return
+		}
+		if buf[0] == 13 {
+			conn.Write([]byte(server.Promote + ">"))
+		}
+		if string(buf[0:3]) == "bye" {
+			conn.Write(buf)
+			break
+		}
+		server.commandHandler.Handle(string(buf))
+	}
 }
 
-func (obj * GoTelnet)DoRun(chanForQuit chan bool){
-    var listener *net.TCPListener
-    for{
-        tcpAddr := obj.getAddr() 
-        var err os.Error
-        listener,err = net.ListenTCP("tcp",tcpAddr)
-        if err != nil{
-            obj.StartPort++
-        } else{
-            break
-        }
-    }
+func (server *GoTelnet) Run() {
+	var listener *net.TCPListener
+	for {
+		tcpAddr := server.getAddr()
+		var err os.Error
+		listener, err = net.ListenTCP("tcp", tcpAddr)
+		if err != nil {
+			server.StartPort++
+		} else {
+			break
+		}
+	}
 
-    
-    chanForAcceptOK := make(chan bool,1)
-    chanForAcceptOK <- true
-    
-    for{ 
-        select {
-        case <-chanForAcceptOK:            
-            go obj.handler(listener,chanForAcceptOK)
-        case <-chanForQuit:
-            fmt.Println("quiting....")
-            return
-        }
-    }    
+	server.chanForNextAccept <- true
+
+	for {
+		select {
+		case <-server.chanForNextAccept:
+			go server.acceptConnection(listener)
+		case <-server.chanForQuit:
+			fmt.Println("quiting....")
+			server.chanForQuitOK <- true
+			return
+		}
+	}
+}
+func (server *GoTelnet) Quit() {
+	server.chanForQuit <- true
+	<-server.chanForQuitOK
 }
--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import "fmt"
-type MyHandler struct{
-    A int
+
+//type CmdFunc func 
+type MyHandler struct {
+	A int
 }
 
 func (obj *MyHandler) Handle(commandLine string) {
-    fmt.Println("recv: " , commandLine)
+	fmt.Println("recv: ", commandLine)
 }
 
-func main(){
-    var handler MyHandler
-    obj := NewGoTelnet("zyn","0.0.0.0",2500,&handler)
+func main() {
+	var handler MyHandler
+	server := NewGoTelnet("zyn", "0.0.0.0", 2500, &handler)
 
-    chanForQuit := make(chan bool,1)
-    go obj.DoRun(chanForQuit)
+	go server.Run()
 
-    for{
-    var quit string
-    fmt.Scanln(&quit)
-    if quit == "quit"{
-        chanForQuit <- true
-        chanForQuit <- true
-        break
-     }   
-   } 
+	for {
+		var quit string
+		fmt.Scanln(&quit)
+		if quit == "quit" {
+			server.Quit()
+			break
+		}
+	}
 }
